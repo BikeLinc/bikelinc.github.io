bfee23e99c1d68e5d1fca30d4e74e86478bcd45d
BikeLinc <lincoln.cobra@gmail.com>
2026-03-11 10:16:48 -0700

    remove un-needed files

--- create_venv.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-VENV_DIR=".venv"
-python3 -m venv "$VENV_DIR"
-source "$VENV_DIR/bin/activate"
-source venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
--- pages/more_coming_soon.md
+++
@@ -1,6 +0,0 @@
-# More coming soon!
-
-**Published:** October 14, 2025   
-**Tags:** Placeholder
-
-More coming soon!
